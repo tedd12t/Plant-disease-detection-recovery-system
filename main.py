5ddda1e2244e648c84a12b30d2ae2e4ef071eb7f
Tedros Nigus Asefa <teddynigus2@gmail.com>
2026-05-10 14:53:04 +0100

    Update tomato disease classifications in main.py

--- main.py
+++ main.py
@@ -741,7 +741,10 @@ elif st.session_state.active_tab_key == "disease_recognition_page_option":
                         'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight',
                         'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy',
                         'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy',
-                        'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                        'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 
+                        'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 
+                        'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 
+                        'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 
                         'Tomato___healthy'
                     ]
                     
